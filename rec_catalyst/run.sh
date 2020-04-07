8a856734b2561dcac58ec17f0e68fd4b8bb84c07
math-vrn <vnikitin90@gmail.com>
2020-04-07 18:07:47 +0200

    add matlab rec

--- rec_catalyst/run.sh
+++ rec_catalyst/run.sh
@@ -3,12 +3,12 @@
 #SBATCH -J laminography
 #SBATCH -p v100
 ##SBATCH --exclude gn1
-#SBATCH -c 32
+#SBATCH -c 42
 #SBATCH --mem 160G
 
 module add GCC/8.3.0  GCCcore/8.3.0  CUDA/10.1.243
 
-CUDA_VISIBLE_DEVICES=0 python rec_align_reg.py 8e-9 &
-CUDA_VISIBLE_DEVICES=1 python rec_align_reg.py 4e-9 &
-wait
+CUDA_VISIBLE_DEVICES=0 python rec_align_matlab.py 
+#CUDA_VISIBLE_DEVICES=1 python rec_align_reg.py 4e-9 &
+#wait
 
